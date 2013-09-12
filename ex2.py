e17a429a2e33770aaf12a38e685ff9ebbc605aea
3catj3 <3catj3@gmail.com>
2013-09-12 15:08:57 +0200

    oppg 2

---
+++ ex2.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+#slik kommenterer man i python.
+#det er viktig og kommentere i koden slik at andre kan forstå hva du tenker osv
+print "dette funker bra" #dette er den eneste linjen man kan se når koden kjøres i shellet!
