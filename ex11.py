7d8358fa6db3fa3454dab073d422df1a14de542e
3catj3 <3catj3@gmail.com>
2013-09-17 13:34:20 +0200

    nye oppg, gjort 10,11,12,13,14

---
+++ ex11.py
@@ -0,0 +1,11 @@
+# -- coding: utf-8 -- 
+
+print "How old are you?",
+age = raw_input(28)
+print "How tall are you?",
+height = raw_input(180)
+print "How much do you weigh?",
+weight = raw_input(hemmelig) #her kommer det opp error, jeg tror jeg kun kan putte inn tall og ikke bokstaver.
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
